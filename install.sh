bc24e511924092a0bf9d3cb27249c7e3d8a163cb
Mr.programmer <78996423+mrprogrammer2938@users.noreply.github.com>
2021-06-09 07:05:30 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,12 +8,14 @@ echo "   ____  ____   __   ____     __  ____  ____  __    ___  __ _ "
 echo "  (    \(    \ /  \ / ___)   / _\(_  _)(_  _)/ _\  / __)(  / )"
 echo "   ) D ( ) D ((  O )\___ \  /    \ )(    )( /    \( (__  )  ( "
 echo "  (____/(____/ \__/ (____/  \_/\_/(__)  (__)\_/\_/ \___)(__\_)"
+echo "   Version: 1.3.0"
 echo ""
 sleep 1
 sudo apt install python
 sudo apt install python-pip
 pip install --upgrade pip
 chmod +x DDos.py
+cd Update && chmod +x update && cd ..
 pip install requirments.txt
 echo ""
 echo "finish..., Installing...!"
