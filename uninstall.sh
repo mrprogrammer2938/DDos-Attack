ee2338fdc8bfccb60f724f20e73b90f1b86d8955
Mr.nope <78996423+mrprogrammer2938@users.noreply.github.com>
2021-07-26 06:52:41 +0430

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 # This code write by Mr.nope
-if [[ "$id -u" -ne 0 ]]; then
-  echo "Please Run This Programm as Root!"
+# DDos-Attacl v1.5.2
+if [[ "$(id -u)" -ne 0 ]]; then
+  echo "
+Please Run This Programm as Root!
+"
   exit 1
 fi
-
-clear
-echo "Uninstalling..."
-sleep 2
-cd .. && rm -r DDos-Attack
-echo ""
-echo "Uninstalling..., Finish...!"
-echo ""
-exit 1
+main() {
+      clear
+      echo "Uninstalling..."
+      sleep 2
+      cd .. && rm -r DDos-Attack
+      echo ""
+      echo "Finish...!"
+      echo "
+Good Bye :(
+"
+      exit 1
+}
+main
